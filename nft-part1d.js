1295254ed12270925b60a7dde3a2d39ad4538a84
Barnabas Zakariya <90233177+barnazaka@users.noreply.github.com>
2024-08-30 04:44:06 +0530

    Create nft-part1d.js

---
+++ nft-part1d.js
@@ -0,0 +1,7 @@
+Create a Non-Fungible Token (NFT)
+These are the images for our NFT collection.
+ Select any image
+
+The images and their metadata live in the InterPlanetary File System (IPFS), which provides decentralized storage. In the next section, we will need the metadata to mint NFTs. For the metadata, we use the standard in this specification. 
+
+These are the content identifiers (CIDs) for the NFT metadata, which points to the images, and below is a sample of the metadata:
