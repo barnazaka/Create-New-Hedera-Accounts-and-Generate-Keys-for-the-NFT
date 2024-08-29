3f8fe9250cc57cc284ee748f2e4c2a65cf6e5cfd
Barnabas Zakariya <90233177+barnazaka@users.noreply.github.com>
2024-08-30 04:44:50 +0530

    Create nft-part1e.js

---
+++ nft-part1e.js
@@ -0,0 +1,25 @@
+// IPFS CONTENT IDENTIFIERS FOR WHICH WE WILL CREATE NFTs - SEE uploadJsonToIpfs.js
+let CIDs = [
+    Buffer.from("ipfs://bafkreibr7cyxmy4iyckmlyzige4ywccyygomwrcn4ldcldacw3nxe3ikgq"),
+    Buffer.from("ipfs://bafkreig73xgqp7wy7qvjwz33rp3nkxaxqlsb7v3id24poe2dath7pj5dhe"),
+    Buffer.from("ipfs://bafkreigltq4oaoifxll3o2cc3e3q3ofqzu6puennmambpulxexo5sryc6e"),
+    Buffer.from("ipfs://bafkreiaoswszev3uoukkepctzpnzw56ey6w3xscokvsvmfrqdzmyhas6fu"),
+    Buffer.from("ipfs://bafkreih6cajqynaqwbrmiabk2jxpy56rpf25zvg5lbien73p5ysnpehyjm"),
+];
+
+{
+  "name": "LEAF1",
+  "creator": "Mother Nature & Hashgraph",
+  "description": "Autumn",
+  "image": "ipfs://Qmb3CMWJzxWZJ34TgJgjASvdTc4x6PEz6LGm2QTWPPpkw5",
+  "type": "image/jpg",
+  "format": "HIP412@2.0.0",
+  "properties": {
+    "city": "Boston",
+    "season": "Fall",
+    "decade": "20's",
+    "license": "MIT-0",
+    "collection": "Fall Collection",
+    "website": "www.hashgraph.com"
+  }
+}
